451096351e2ddcdb7f4ad2acde442590c48b19cd
av <45238682+avxlji@users.noreply.github.com>
2020-10-24 15:17:57 -0400

    added alan-ai voice and updated dashboard

---
+++ client/src/components/layout/dashboard/index.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = 'a36736f23b21bfd709dcc410696ad0a52e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const Dashboard = () => {
+      // initialize voice
+  useEffect(() => {
+    alanBtn({
+        key: alanKey,
+        onCommand: ({ command }) => {
+            switch (command){
+
+            }
+        }
+    })
+}, []);
+    return (
+        <div>
+            dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Register from "../auth/register";
 import Login from "../auth/login";
+import PrivateRoute from './PrivateRoute';
+import Dashboard from '../layout/dashboard'
 
 const Routes = () => {
   return (
@@ -9,18 +11,7 @@ const Routes = () => {
       <Switch>
       <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        {/* <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/profiles" component={Profiles} />
-        <Route exact path="/profile/:id" component={Profile} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-        <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-        <PrivateRoute exact path="/add-experience" component={AddExperience} />
-        <PrivateRoute exact path="/add-education" component={AddEducation} />
-        <PrivateRoute exact path="/posts" component={Posts} />
-        <PrivateRoute exact path="/posts/:id" component={Post} />
-        <Route component={NotFound} /> */}
       </Switch>
     </section>
   );
