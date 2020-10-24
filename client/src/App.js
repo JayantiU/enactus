bb6e0ea98b57a352f1f6dc3971d6d511feb4dee2
av <45238682+avxlji@users.noreply.github.com>
2020-10-24 14:55:17 -0400

    set up redux lifecycle and implemented auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Routes from "./components/routing/Routes";
+import Landing from "./components/layout/landing";
+import Navbar from './components/layout/navbar'
 
+//Redux imports
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => {
-
-    
-    return (
-        <p>hello world</p>
-    )
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
 
-export default App;
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+          <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ client/src/components/layout/navbar/index.js
@@ -0,0 +1,75 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../../actions/auth";
+
+//material UI imports
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import styles from './index.module.css';
+
+const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
+  const authLinks = (
+    <ul id={styles.navOptions}>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span>Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a href="!#" onClick={logout}>
+          <i className="fas fa-sign-out-alt" />{" "}
+          <span>Logout</span>{" "}
+          {/* hide-sm hides text on smaller screens */}
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul id={styles.navOptions}>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
+
+  return (
+    <nav id={styles.navContainer}>
+            <Toolbar id={styles.toolbar}>
+            <div id={styles.headerLeft}>
+            <Link to="/">
+          enactus
+        </Link>
+          </div>
+
+          <div id={styles.headerRight}>
+            <>
+            {!loading && (
+        <div>{isAuthenticated ? authLinks : guestLinks}</div>
+      )}
+            </>
+          </div>
+      {/* If not loading, evaluate fragment */}
+   
+      </Toolbar>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
